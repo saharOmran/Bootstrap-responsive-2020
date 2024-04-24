f193abbf7045b7e399b0cfbca9dabce74cfecccb
saharomran <saharomraan@gmail.com>
2024-04-24 21:16:52 +0330

    update navbar

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+function toggleVisibility() {
+    var element = document.getElementById('side');
+    if (element.style.display === 'none') {
+        element.style.display = 'block';
+    } else {
+        element.style.display = 'none';
+    }
+}
